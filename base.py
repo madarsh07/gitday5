fd5d843ea01d488e666a6ba3212ac866b6b789e6
madarsh07 <adarshmendhe101995@gmail.com>
2025-07-02 17:42:57 +0530

    appcode

---
+++ base.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+
+def greet_user():
+    now = datetime.now()
+    current_hour = now.hour
+
+    if current_hour < 12:
+        greeting = "Good morning"
+    elif current_hour < 17:
+        greeting = "Good afternoon"
+    else:
+        greeting = "Good evening"
+
+    print(f"{greeting}! The current date and time is {now.strftime('%Y-%m-%d %H:%M:%S')}")
+
+greet_user()
+
